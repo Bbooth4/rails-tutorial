91099070732aa656ed863d2b6e26352b68cf7147
Brandon <booth.brandon4@gmail.com>
2017-05-29 16:59:43 -0400

    can answer questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,7 +4,7 @@ class AnswersController < ApplicationController
     question = Question.find(params[:answer][:question_id])
     answer = question.answers.create(answer_params)
 
-    MainMailer.notify_question_author(answer).deliver_later
+    # MainMailer.notify_question_author(answer).deliver_later
 
     session[:current_user_email] = answer_params[:email]
     redirect_to question
@@ -13,7 +13,7 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:answer).permit(:email, :body)
+    params.require(:answer).permit(:question_id, :email, :body)
   end
 
 end
